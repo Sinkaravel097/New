8e71565e0df6f8440cb744410cb2969c896d9baf
Sinkaravel097 <sinkaravel.career@gmail.com>
2021-01-13 14:48:42 +0530

    new

---
+++ src/org/file/ope/FileOpe.java
@@ -0,0 +1,37 @@
+package org.file.ope;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileOpe {
+
+	static {
+
+		System.out.println("Output");
+	}
+
+	// public static void main(String[] args) throws IOException {
+	//
+	// File f = new File("E:\\Sel\\newfolder\\sel.exe");
+	//
+	// boolean mkdir = f.mkdir();
+	//
+	// System.err.println("dir : " + mkdir);
+	//
+	// boolean mkdirs = f.mkdirs();
+	//
+	// System.out.println("dirs :" + mkdirs);
+	//
+	// boolean newFi = f.createNewFile();
+	//
+	// System.out.println("file :" + newFi);
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	// }
+
+}
